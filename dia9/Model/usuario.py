97557c09884ed0d9b74efb62e454f677f1ae3713
dlarenasd <d.larenas.d@gmail.com>
2024-04-18 13:05:20 -0400

    dia8: diagramas, día9: desafío diagramas

---
+++ dia9/Model/usuario.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Usuario:
+    def __init__(self, correo, edad, region):
+        self.__correo = correo
+        self.__edad = edad
+        self.__region = region
+
+    def contesta_encuesta(self, encuesta):
+        pass
+
